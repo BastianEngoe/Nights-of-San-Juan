f1e00c0032cf14c92d0cb06d587f8c576c25c8fd
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2023-09-06 11:49:13 +0200

    Weather System WIP 1

---
+++ Assets/Editor/WeatherSystemEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WeatherSystem))]
+
+public class WeatherSystemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WeatherSystem weatherSystem = (WeatherSystem)target;
+    }
+}
