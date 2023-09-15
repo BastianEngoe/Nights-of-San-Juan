121eb46f31ac5897fb25252a9f645502d206d4dc
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2023-09-15 10:39:49 +0200

    Update Dialogue.cs

--- Assets/_Scripts/Systems/DialogueSystem/Dialogue.cs
+++ Assets/_Scripts/Systems/DialogueSystem/Dialogue.cs
@@ -7,6 +7,8 @@ public class Dialogue
 {
     public string[] strings {get; private set;}
     public bool canRespond {get; private set;}
+    
+    public Transform currentSpeaker { get; }
     public Response[] responses {get; private set;}
     public Dialogue(string[] strings) {
         this.strings = strings;
@@ -17,6 +19,7 @@ public class Dialogue
 
 public class Response{
     public string responseText {get; private set;}
+    public Transform responseSpeaker { get; private set; }
     public Action<Dialogue> response {get; private set;}
 
 }
