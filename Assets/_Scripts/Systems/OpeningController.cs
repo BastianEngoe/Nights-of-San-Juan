444d3678d2f82616a535ae9789d43297ad4cdc2d
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2023-09-29 11:46:09 +0200

    Luarca overhaul and intro

---
+++ Assets/_Scripts/Systems/OpeningController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class OpeningController : MonoBehaviour
+{
+    public CanvasGroup TitleCanvas;
+    [SerializeField] private float cAlpha = 1, t;
+    private bool startFade;
+    public PlayableDirector cutController;
+    public Animator beaAnim;
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            startFade = true;
+
+            Invoke("StartCutscene", 2.5f);
+        }
+
+        if (startFade)
+        {
+            cAlpha = Mathf.Lerp(1, 0, t);
+            t += 0.75f * Time.deltaTime;
+            TitleCanvas.alpha = cAlpha;
+        }
+    }
+
+    void StartCutscene()
+    {
+        cutController.Play();
+        beaAnim.enabled = true;
+        Invoke("BeaLook", 4f);
+    }
+    void BeaLook()
+    {
+        beaAnim.SetBool("Look", true);
+    }
+}
