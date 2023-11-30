85d3c9c4c7779b7e9fe27dcb0b3b5f52b21f7e67
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2023-11-30 15:19:06 +0100

    Font updates!

--- Assets/_Scripts/Systems/UI/PickupText.cs
+++ Assets/_Scripts/Systems/UI/PickupText.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -5,13 +6,20 @@ using UnityEngine;
 public class PickupText : MonoBehaviour
 {
     [SerializeField] private TMP_Text pickupText;
-    
+
+    private Vector3 startPosition;
+
+    public void Awake()
+    {
+        startPosition = transform.position;
+    }
+
     public void ItemText(string item)
     {
         pickupText.text = "Picked up " + item;
 
         pickupText.DOFade(1f, 0f);
         pickupText.rectTransform.DOMove(new Vector3(pickupText.rectTransform.position.x, pickupText.rectTransform.position.y + 200, pickupText.rectTransform.position.x), 2f);
-        pickupText.DOFade(0f, 3f);
+        pickupText.DOFade(0f, 3f).OnComplete((() => transform.position = startPosition));
     }
 }
